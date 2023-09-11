28ccf4aa566a8eb066915b9b53406d363453ecc5
Andrey Talanin <andrey.talanin@outlook.com>
2023-09-11 21:55:45 +0300

    Add the generateHieroglyphWordExercise Typescript API request function

--- Source/HieroglyphicWritingExerciseTool/ClientApplication/src/api/requests.ts
+++ Source/HieroglyphicWritingExerciseTool/ClientApplication/src/api/requests.ts
@@ -1,10 +1,21 @@
 import axios from "axios";
 import { GenerateHieroglyphExerciseRequest } from "../models/GenerateHieroglyphExerciseRequest";
 import { GenerateHieroglyphExerciseResponse } from "../models/GenerateHieroglyphExerciseResponse";
+import { GenerateHieroglyphWordExerciseRequest } from "../models/GenerateHieroglyphWordExerciseRequest";
+import { GenerateHieroglyphWordExerciseResponse } from "../models/GenerateHieroglyphWordExerciseResponse";
 
 export async function generateHieroglyphExercise(request: GenerateHieroglyphExerciseRequest): Promise<GenerateHieroglyphExerciseResponse> {
   try {
     const { data } = await axios.post(`/api/Exercise/GenerateHieroglyphExercise`, request);
+    return data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+
+export async function generateHieroglyphWordExercise(request: GenerateHieroglyphWordExerciseRequest): Promise<GenerateHieroglyphWordExerciseResponse> {
+  try {
+    const { data } = await axios.post(`/api/Exercise/GenerateHieroglyphWordExercise`, request);
     return data;
   } catch (error) {
     return Promise.reject(error);
