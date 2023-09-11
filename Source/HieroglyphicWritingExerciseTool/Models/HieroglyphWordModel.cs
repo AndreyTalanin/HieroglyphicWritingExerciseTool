de6fcbfe6f3c36991f338d2396516c806e8f22d4
Andrey Talanin <andrey.talanin@outlook.com>
2023-09-11 20:47:03 +0300

    Add the HieroglyphWordModel model class

---
+++ Source/HieroglyphicWritingExerciseTool/Models/HieroglyphWordModel.cs
@@ -0,0 +1,12 @@
+namespace HieroglyphicWritingExerciseTool.Models;
+
+public class HieroglyphWordModel
+{
+    public string Type { get; set; } = string.Empty;
+
+    public string Characters { get; set; } = string.Empty;
+
+    public string Pronunciation { get; set; } = string.Empty;
+
+    public string Meaning { get; set; } = string.Empty;
+}
