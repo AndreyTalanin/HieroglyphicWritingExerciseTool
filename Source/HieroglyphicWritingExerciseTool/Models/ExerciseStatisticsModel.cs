eefd7b84cf27c89f279301a831936108c8769905
Andrey Talanin <andrey.talanin@outlook.com>
2023-09-24 19:32:00 +0300

    Add the ExerciseStatisticsModel model class

---
+++ Source/HieroglyphicWritingExerciseTool/Models/ExerciseStatisticsModel.cs
@@ -0,0 +1,12 @@
+namespace HieroglyphicWritingExerciseTool.Models;
+
+public class ExerciseStatisticsModel
+{
+    public double CurrentTimeMilliseconds { get; set; }
+
+    public double AverageTimeMilliseconds { get; set; }
+
+    public double MinTimeMilliseconds { get; set; }
+
+    public double MaxTimeMilliseconds { get; set; }
+}
