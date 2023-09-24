f7ad6c767814aca5e9c0d9d6eb8b4c815c090e01
Andrey Talanin <andrey.talanin@outlook.com>
2023-09-24 18:54:09 +0300

    Add the StatisticsStorageConfiguration class

---
+++ Source/HieroglyphicWritingExerciseTool/Configuration/StatisticsStorageConfiguration.cs
@@ -0,0 +1,6 @@
+namespace HieroglyphicWritingExerciseTool.Configuration;
+
+public class StatisticsStorageConfiguration
+{
+    public string StatisticsStorageFileName { get; set; } = string.Empty;
+}
